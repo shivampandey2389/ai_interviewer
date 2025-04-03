6f40f0cd62629d7a87c084f5ca31ad3b87f5e120
shivampandey2389 <shivampandey2389@gmail.com>
2025-04-03 21:58:19 +0530

    setup auth functionality

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+const RootLayout = ({children}:{children:ReactNode}) =>{
+  return <div>{children}</div>
+}
+export default RootLayout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Page = () =>{
+  return <h1>Home Page</h1>
+}
+export default Page;
