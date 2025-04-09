f3a50f2d536b1170486b83b46a8eb2ef52d3491c
shivampandey2389 <shivampandey2389@gmail.com>
2025-04-09 21:54:54 +0530

    created interview page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/Agent";
+
+const page =()=>{
+  return(
+    <>
+    <h3>Interview Generation</h3>
+    <Agent userName={"You"}/>
+    </>
+  )
+}
+export default page;
